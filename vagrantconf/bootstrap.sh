7c8825b16ab81923627a06bfe63d399cc4673683
asiemons <asiemons@live.com>
2014-10-18 11:57:43 +0200

    change modx latest url

--- vagrantconf/bootstrap.sh
+++ vagrantconf/bootstrap.sh
@@ -27,7 +27,7 @@ echo 'MySQL config\nuser: root\npassword: root'
 
 # modx
 echo "Getting MODx..."
-wget -q -O modx.zip http://modx.com/download/latest/latest.zip
+wget -q -O modx.zip http://modx.com/download/latest/
 
 # unzip and put to place
 unzip -q modx.zip
